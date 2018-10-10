869f1b4c38b65490aa8585e6e37501846c8b0fc9
Thomas "Macouta" Blanc <thomas.hakunamacouta.blanc@gmail.com>
2018-10-10 12:34:15 +0200

    boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,10 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi' : [2, 'never'],
+    'indent' : ['error', 'tab'],
+    'no-tabs' : 0
   },
   parserOptions: {
     parser: 'babel-eslint'
