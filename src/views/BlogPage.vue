71404e4bea9cacb6b94383d28cc1d80888e6a151
Thomas "Macouta" Blanc <thomas.hakunamacouta.blanc@gmail.com>
2018-10-22 16:08:32 +0200

    feat(BlogPage): add disqus comment for each blog article

--- src/views/BlogPage.vue
+++ src/views/BlogPage.vue
@@ -3,13 +3,7 @@
     <h1>{{ post.data.title }}</h1>
     <h4>{{ post.data.author.first_name }} {{ post.data.author.last_name }}</h4>
     <div v-html="post.data.body"></div>
-
-    <router-link v-if="post.meta.previous_post" :to="/blog/ + post.meta.previous_post.slug" class="button">
-      {{ post.meta.previous_post.title }}
-    </router-link>
-    <router-link v-if="post.meta.next_post" :to="/blog/ + post.meta.next_post.slug" class="button">
-      {{ post.meta.next_post.title }}
-    </router-link>
+    <vue-disqus shortname="hakunamacouta" :identifier="this.$route.params.slug"></vue-disqus>
   </div>
 </template>
 
@@ -26,10 +20,8 @@ export default {
     getPost() {
       butter.post.retrieve(this.$route.params.slug)
         .then((res) => {
-          // console.log(res.data)
           this.post = res.data
         }).catch((res) => {
-          console.log(res)
         })
     }
   },
